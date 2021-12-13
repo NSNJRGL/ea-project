1bf3503fa877cafaa605a861a940e06b55ad09d8
yohannes84 <34288731+yohannes84@users.noreply.github.com>
2021-12-12 21:24:10 -0600

    Yohannes Comminting first

--- src/main/java/cs544/team1/model/Audit.java
+++ src/main/java/cs544/team1/model/Audit.java
@@ -1,27 +1,2 @@
-package cs544.team1.model;
-
-import java.time.LocalDateTime;
-
-import javax.persistence.Embeddable;
-
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
-@Embeddable
-public class Audit {
-	@CreationTimestamp
-	private LocalDateTime createdAt;
-	@UpdateTimestamp
-	private LocalDateTime updatedAt;
-	private Person createdBy;
-	private Person modifiedBy;
-	
-	public Audit(Person createdBy, Person modifiedBy) {
-		this.createdBy = createdBy;
-		this.modifiedBy = modifiedBy;
-	}
-	
-	public Audit(Person modifiedBy) {
-		this.modifiedBy = modifiedBy;
-	}
+package cs544.team1.model;public class Audit {
 }
